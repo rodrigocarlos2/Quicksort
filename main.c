290563e31a4b37a366d7a1b16f65560e12af8709
rodrigocarlos2 <rodrigo19962010@live.com>
2016-06-04 22:37:17 -0300

    Error resolved in algorithm - Rodrigo Carlos

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,30 +13,36 @@ void swap(int *a, int *b){
 }
 
 int part(int *vetor, int leaf, int right){
+	
     int pivo =  vetor[leaf];
+    
     int j, i = leaf;
 
     for(j=leaf+1; j<right; j++){
-        if(vetor[j]< pivo){
+    	
+        if(vetor[j]<pivo){
             i++;
             swap(&vetor[i], &vetor[j]);
         }
+        
     }
+    
     swap(&vetor[leaf], &vetor[i]);
 
     return i;
-
-
 }
 
 void quicksort(int *vetor, int inicio, int fim){
 
     int partition;
 
-    if(inicio< fim){
+    if(inicio<fim){
+    	
         partition = part(vetor, inicio, fim);
-        quicksort(vetor,inicio, partition-1);
-        quicksort(vetor,partition+1,fim);
+        
+        quicksort(vetor, inicio, partition);
+        quicksort(vetor, partition+1, fim);
+        
     }
 
 }
@@ -42,23 +50,30 @@ void quicksort(int *vetor, int inicio, int fim){
 int main(){
 
     int vetor[]={100, 23, 32, 65, 7, 8, 98, 123, 34, 92, 54, 33, 45, 4, 3, 2, 1};
+    //int vetor[] = {100, 23, 2, 1};
     int tam = sizeof(vetor)/sizeof(int);
     int i;
 
     for(i=0; i<tam; i++){
-        printf("%d ", vetor[i]);
+        if(i!=tam-1)
+        	printf("%d - ", vetor[i]);
+        else
+        	printf("%d", vetor[i]);
     }
-
-    printf("\n");
 
     quicksort(vetor,0, tam);
 
-    printf("\n");
-
-
     for(i=0; i<tam; i++){
-        printf("%d ", vetor[i]);
+    	if(i!=tam-1)
+        	printf("%d - ", vetor[i]);
+        else
+        	printf("%d", vetor[i]);
     }
 
+	printf("\n");
+
+	system("pause");
+
+	return 0;
 
 }
